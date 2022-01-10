b4d62da6f13fb56bec9da8a4968eb9facac42f34
BrookeBryski <brookeabryski@gmail.com>
2022-01-10 14:23:26 -0600

    initial commit

---
+++ src/main/java/com/example/ChicagoRestaurants/ChicagoRestaurantsApplication.java
@@ -0,0 +1,13 @@
+package com.example.ChicagoRestaurants;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChicagoRestaurantsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChicagoRestaurantsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ChicagoRestaurants/ChicagoRestaurantsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ChicagoRestaurants;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChicagoRestaurantsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
