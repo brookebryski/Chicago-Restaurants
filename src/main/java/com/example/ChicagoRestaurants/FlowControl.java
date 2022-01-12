943ff64b321181f46ed6fe856da4a056df7989c4
BrookeBryski <brookeabryski@gmail.com>
2022-01-12 14:19:22 -0600

    flow control class

--- src/main/java/com/example/ChicagoRestaurants/FlowControl.java
+++ src/main/java/com/example/ChicagoRestaurants/FlowControl.java
@@ -1,4 +1,18 @@
 package com.example.ChicagoRestaurants;
 
-public class FlowControl {
+public class FlowControl implements IFlowControl {
+    private IRestaurantValidator _restaurantValidator;
+    private IRestaurant _restaurant;
+
+    public FlowControl(IRestaurantValidator restaurantValidator, IRestaurant restaurant) {
+        this._restaurantValidator = restaurantValidator;
+        this._restaurant = restaurant;
+    }
+
+    public void run(String input) {
+        if(_restaurantValidator.isRestaurant(input)) {
+            String result = _restaurant.generateRestaurant(input);
+            System.out.println(result);
+        }
+    }
 }
